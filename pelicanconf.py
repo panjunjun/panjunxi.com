240a4ecab52d52e661183739c199ffe2cb3f736f
潘俊俊 <keyps2@icloud.com>
2017-07-05 17:14:55 +0800

    翻译模板的部分内容、调整一些显示格式、添加骚气的favicon

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,49 +2,39 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'潘俊xi'
-SITENAME = u'潘俊xi'
-SITEURL = ''
+AUTHOR = u"潘俊xi"
+SITENAME = u"潘俊xi"
+SITEURL = ""
 
-PATH = 'content'
+PATH = "content"
 
-TIMEZONE = 'Asia/Shanghai'
+TIMEZONE = "Asia/Shanghai"
 
-DEFAULT_LANG = u'zh'
-PAGE_LANG_URL = 'pages/{slug}-{lang}.html'
+DEFAULT_LANG = u"zh"
+PAGE_LANG_URL = "pages/{slug}-{lang}.html"
 
-# Feed generation is usually not desired when developing
+
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
+
 LINKS = (
-    ('Github', 'https://github.com/panjunjun'),
-    # ('Python.org', 'http://python.org/'),
-    # ('Jinja2', 'http://jinja.pocoo.org/'),
+    ("Github", "https://github.com/panjunjun"),
 )
 
-# Social widget
 SOCIAL = (
-    ('LinkedIn', '#'),
-    # ('Another social link', '#'),
+    ("LinkedIn", "#"),
 )
 
 DEFAULT_PAGINATION = 10
-# BLOG_AUTHORS = dict(
-#     panjunxi="潘俊xi"
-# )
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
-# THEME_STATIC_DIR = './theme/pelican-themes'
+
 THEME = "theme/pelican-themes/zurb-F5-basic"
-# DISPLAY_TAGS_ON_SIDEBAR_LIMIT = 10
-# DISPLAY_LINKS_ON_SIDEBAR_LIMIT = 10
-# LICENSE = "nothing"
-MARKUP = ('md', 'ipynb')
-PLUGIN_PATHS = ['./plugins']
-PLUGINS = ['ipynb.markup']
-IGNORE_FILES = ['.ipynb_checkpoints']
+
+MARKUP = ("md", "ipynb")
+PLUGIN_PATHS = ["./plugins"]
+PLUGINS = ["ipynb.markup"]
+IGNORE_FILES = [".ipynb_checkpoints"]
+DEFAULT_DATE_FORMAT = "%Y-%m-%d %H:%M"
